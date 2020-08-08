f7a438c93ecbd9681ec1e04f9b5b0f819bda6003
ADaouadji <66480400+ADaouadji@users.noreply.github.com>
2020-08-08 18:51:13 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 // Create a react component
-const App = () =>{
-    return <div>Hi there !</div>;
+const App = () => {
+  const buttonText = { text: 'Click me' };
+  const labelText = 'Enter name:';
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
 };
+
 
 // Take the react component and show it on the screen
 ReactDom.render(
